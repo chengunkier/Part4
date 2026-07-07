a214357b321908c80d0632b66bbe8503000b8bc9
chengunkier <inabamicheal97@gmail.com>
2026-07-07 12:42:21 +0300

    updates for part5 exercise 5.17: blog list end to end tests, step1

--- app.js
+++ app.js
@@ -26,6 +26,12 @@ app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
+// only enable testing router in test/development environment
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
